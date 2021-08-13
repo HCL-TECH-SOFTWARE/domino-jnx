2ff75bafa2da91a52ae12aa5897ae22339eb641e
Jesse Gallagher <jesse@secondfoundation.org>
2021-08-13 14:18:10 -0400

    Modify testViewSerialization to run on a collection of common templates

--- test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/vertx/json/TestVertxDesignJsonSerialization.java
+++ test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/vertx/json/TestVertxDesignJsonSerialization.java
@@ -1,6 +1,7 @@
 package it.com.hcl.domino.test.vertx.json;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.hcl.domino.data.Database;
 import com.hcl.domino.jnx.vertx.json.service.VertxJsonSerializer;
@@ -13,11 +14,23 @@ import it.com.hcl.domino.test.AbstractNotesRuntimeTest;
  */
 public class TestVertxDesignJsonSerialization extends AbstractNotesRuntimeTest {
 
-  @Test
-  public void testViewSerialization() {
+  // Run against a handful of common stock NTFs to get a spread of examples
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "pernames.ntf",
+    "bookmark.ntf",
+    "log.ntf",
+    "mailbox.ntf",
+    "roamingdata.ntf",
+    "autosave.ntf",
+    "doclbs7.ntf",
+    "headline.ntf",
+    "busytime.ntf"
+  })
+  public void testViewSerialization(String dbName) {
     VertxJsonSerializer serializer = new VertxJsonSerializer();
     
-    Database names = getClient().openDatabase("names.nsf");
+    Database names = getClient().openDatabase(dbName);
     names.getDesign()
       .getViews()
       .forEach(view -> {
