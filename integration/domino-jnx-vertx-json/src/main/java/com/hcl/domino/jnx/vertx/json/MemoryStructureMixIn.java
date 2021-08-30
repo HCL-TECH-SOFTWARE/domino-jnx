daa5a87de624dd010c913f8f073103ef455e59e5
Karsten Lehmann <karsten.lehmann@mindoo.de>
2021-08-30 12:53:46 +0200

    Merged from develop

--- integration/domino-jnx-vertx-json/src/main/java/com/hcl/domino/jnx/vertx/json/MemoryStructureMixIn.java
+++ integration/domino-jnx-vertx-json/src/main/java/com/hcl/domino/jnx/vertx/json/MemoryStructureMixIn.java
@@ -1,9 +1,10 @@
 package com.hcl.domino.jnx.vertx.json;
 
 import java.nio.ByteBuffer;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public abstract class MemoryStructureMixIn {
+  
   @JsonIgnore abstract ByteBuffer getData();
+
 }
