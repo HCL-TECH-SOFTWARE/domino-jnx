65eb4c6cfc75242ff11a059998c17eaa04d50319
Jesse Gallagher <jesse@secondfoundation.org>
2021-10-18 16:21:25 -0400

    SWT Example: Add DXL export to document info

--- example/jnx-example-swt/src/main/java/com/hcl/domino/jnx/example/swt/doc/DocumentInfoPane.java
+++ example/jnx-example-swt/src/main/java/com/hcl/domino/jnx/example/swt/doc/DocumentInfoPane.java
@@ -16,8 +16,6 @@
  */
 package com.hcl.domino.jnx.example.swt.doc;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
@@ -25,13 +23,14 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Text;
 
 import com.hcl.domino.DominoClient;
 import com.hcl.domino.data.Database;
 import com.hcl.domino.data.Document;
-import com.hcl.domino.data.Item;
-import com.hcl.domino.data.ItemDataType;
+import com.hcl.domino.dxl.DxlExporter;
 import com.hcl.domino.jnx.example.swt.info.AbstractInfoPane;
 
 import jakarta.enterprise.inject.spi.CDI;
@@ -39,55 +38,78 @@ import jakarta.json.bind.Jsonb;
 
 public class DocumentInfoPane extends AbstractInfoPane {
 
-  private Text editor;
-
   public DocumentInfoPane(final Composite parent, final String serverName, final String databasePath, final String unid) {
     super(parent, unid);
-
-    this.info("UNID", unid); //$NON-NLS-1$
+    
     final ExecutorService exec = CDI.current().select(ExecutorService.class).get();
 
+    this.info("UNID", unid); //$NON-NLS-1$
+    
     try {
-      @SuppressWarnings("unchecked")
-      final List<String> categories = exec.submit(() -> {
+      int noteId = exec.submit(() -> {
         final DominoClient client = CDI.current().select(DominoClient.class).get();
         final Database database = client.openDatabase(serverName, databasePath);
-        final Document doc = database.getDocumentByUNID(unid).get();
-        final Item catItem = doc.getFirstItem("Categories").orElse(null); //$NON-NLS-1$
-
-        if (catItem != null && (catItem.getType() == ItemDataType.TYPE_TEXT || catItem.getType() == ItemDataType.TYPE_TEXT_LIST)) {
-          return catItem.getAsList(String.class, Collections.emptyList());
-        } else {
-          return Collections.EMPTY_LIST;
-        }
+        return database.toNoteId(unid);
       }).get();
+      this.info("Note ID", "0x" + Integer.toHexString(noteId));
+      
+      TabFolder tabFolder = new TabFolder(this, SWT.NONE);
+      tabFolder.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 2, 1));
+      
+      // JSON Export
+      {
+        TabItem tab = new TabItem(tabFolder, SWT.NONE);
+        tab.setText("JSON");
+        
+        Text editor = new Text(tabFolder, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.WRAP | SWT.BORDER);
+        tab.setControl(editor);
+        editor.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        // TODO pick a non-named font -
+        // https://stackoverflow.com/questions/221568/swt-os-agnostic-way-to-get-monospaced-font
+        editor.setFont(new Font(parent.getDisplay(), "Consolas", 12, SWT.NORMAL)); //$NON-NLS-1$
 
-      this.info("Categories", categories);
+        final Jsonb jsonb = CDI.current().select(Jsonb.class).get();
+        final String json = exec.submit(() -> {
+          final DominoClient client = CDI.current().select(DominoClient.class).get();
+          final Database database = client.openDatabase(serverName, databasePath);
+          final Document doc = database.getDocumentByUNID(unid).get();
+          return jsonb.toJson(doc);
+        }).get();
+        editor.setText(json);
 
-      this.editor = new Text(this, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.WRAP | SWT.BORDER);
-      this.editor.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-      // TODO pick a non-named font -
-      // https://stackoverflow.com/questions/221568/swt-os-agnostic-way-to-get-monospaced-font
-      this.editor.setFont(new Font(parent.getDisplay(), "Consolas", 12, SWT.NORMAL)); //$NON-NLS-1$
+        editor.clearSelection();
+      }
+      
+      // DXL Export
+      {
+        TabItem tab = new TabItem(tabFolder, SWT.NONE);
+        tab.setText("DXL");
+        
+        Text editor = new Text(tabFolder, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.WRAP | SWT.BORDER);
+        tab.setControl(editor);
+        editor.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+        // TODO pick a non-named font -
+        // https://stackoverflow.com/questions/221568/swt-os-agnostic-way-to-get-monospaced-font
+        editor.setFont(new Font(parent.getDisplay(), "Consolas", 12, SWT.NORMAL)); //$NON-NLS-1$
 
-      final Jsonb jsonb = CDI.current().select(Jsonb.class).get();
-      final String json = exec.submit(() -> {
-        final DominoClient client = CDI.current().select(DominoClient.class).get();
-        final Database database = client.openDatabase(serverName, databasePath);
-        final Document doc = database.getDocumentByUNID(unid).get();
-        return jsonb.toJson(doc);
-      }).get();
-      this.editor.setText(json);
+        // TODO delay until display
+        final String dxl = exec.submit(() -> {
+          final DominoClient client = CDI.current().select(DominoClient.class).get();
+          final Database database = client.openDatabase(serverName, databasePath);
+          final Document doc = database.getDocumentByUNID(unid).get();
+          DxlExporter exporter = client.createDxlExporter();
+          return exporter.exportDocument(doc);
+        }).get();
+        editor.setText(dxl);
+
+        editor.clearSelection();
+      }
+      
       this.layout();
 
-      this.editor.clearSelection();
     } catch (ExecutionException | InterruptedException e) {
       throw new RuntimeException(e);
     }
-  }
-
-  public String getJsonString() {
-    return this.editor.getText();
   }
 
 }
