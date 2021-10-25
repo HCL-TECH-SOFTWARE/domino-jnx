1398c72d1b8980a415f5354f0cb7725b58bc73d6
shilpa.sm <shilpa.sm@hcl.com>
2021-10-25 17:49:09 +0530

    Implement ViewmapButtonDefaults 118

--- test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/richtext/TestRichTextRecords.java
+++ test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/richtext/TestRichTextRecords.java
@@ -106,6 +106,7 @@ import com.hcl.domino.richtext.records.CurrencyType;
 import com.hcl.domino.richtext.records.RecordType;
 import com.hcl.domino.richtext.records.RecordType.Area;
 import com.hcl.domino.richtext.records.RichTextRecord;
+import com.hcl.domino.richtext.records.ViewmapButtonDefaults;
 import com.hcl.domino.richtext.structures.AssistFieldStruct;
 import com.hcl.domino.richtext.structures.AssistFieldStruct.ActionByField;
 import com.hcl.domino.richtext.structures.CDPoint;
@@ -1413,6 +1414,31 @@ public class TestRichTextRecords extends AbstractNotesRuntimeTest {
 
         @SuppressWarnings("unused")
         final CDLayoutButton begin = (CDLayoutButton) doc.getRichTextItem("Body").get(0);
+      });
+    }
+    
+
+    @Test
+    public void testVMButtonDefaults() throws Exception {
+      this.withTempDb(database -> {
+        final Document doc = database.createDocument();
+        try (RichTextWriter rtWriter = doc.createRichTextItem("Body")) {
+          final ViewmapButtonDefaults begin =rtWriter.createStructure(ViewmapButtonDefaults.class, 0);           
+            begin.setLineColor(207);
+            begin.setFillFGColor(15);
+            begin.setFillBGColor(15);
+            begin.setLineStyle(0);
+            begin.setLineWidth(1);
+            begin.setFillStyle(1);
+
+            assertEquals(207, begin.getLineColor());
+            assertEquals(15,begin.getFillFGColor());
+            assertEquals(15,begin.getFillBGColor());
+            assertEquals(0,begin.getLineStyle());
+            assertEquals(1,begin.getLineWidth());
+            assertEquals(1,begin.getFillStyle());
+        }
+        
       });
     }
 }
