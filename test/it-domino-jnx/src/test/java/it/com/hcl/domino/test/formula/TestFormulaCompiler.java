81fdeec65cdc59c8fc85e31f9d51cb5f42c6eb65
Karsten Lehmann <karsten.lehmann@mindoo.de>
2022-03-18 14:21:46 +0100

    API to apply security to formula execution

--- test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/formula/TestFormulaCompiler.java
+++ test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/formula/TestFormulaCompiler.java
@@ -17,8 +17,10 @@
 package it.com.hcl.domino.test.formula;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Stream;
@@ -32,8 +34,11 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import com.hcl.domino.DominoClient;
+import com.hcl.domino.DominoException;
+import com.hcl.domino.commons.errors.INotesErrorConstants;
 import com.hcl.domino.data.Document;
 import com.hcl.domino.data.Formula;
+import com.hcl.domino.data.Formula.Disallow;
 import com.hcl.domino.data.Formula.FormulaExecutionResult;
 import com.hcl.domino.data.FormulaAnalyzeResult.FormulaAttributes;
 import com.hcl.domino.formula.FormulaCompiler;
@@ -153,8 +158,175 @@ public class TestFormulaCompiler extends AbstractNotesRuntimeTest {
         FormulaExecutionResult resultDeletion = formulaDeletion.evaluateExt(doc);
         assertEquals( true, resultDeletion.shouldBeDeleted(), "Formula marked doc for deletion");
       }
-
       
     });
+  }
+  
+  @Test
+  public void testDisallowedActions() throws Exception {
+    withTempDb((db) -> {
+      DominoClient client = getClient();
+      Document otherDoc = db.createDocument();
+      otherDoc.replaceItemValue("Form", "Testform");
+      otherDoc.save();
+      String otherDocUnid = otherDoc.getUNID();
+      otherDoc.autoClosable().close();
+      
+      Document doc = db.createDocument();
+
+      //check if we can prevent Notes.ini changes
+      {
+        client.getDominoRuntime().setProperty("$jnx_test", "");
+
+        Formula formula = client
+            .createFormula("@SetEnvironment(\"jnx_test\"; \"123\")");
+        formula.evaluate();
+        //this is allowed, to the value should have been set
+        String val = client.getDominoRuntime().getPropertyString("$jnx_test");
+        assertEquals("123", val);
+      }
+      
+      {
+        client.getDominoRuntime().setProperty("$jnx_test", "");
+        
+        DominoException e = null;
+        try {
+          Formula formula = client
+              .createFormula("@SetEnvironment(\"jnx_test\"; \"123\")")
+              .disallow(Disallow.SETENVIRONMENT);
+          formula.evaluate();
+        }
+        catch (DominoException e1) {
+          e = e1;
+        }
+
+        assertNotNull(e);
+        assertTrue(e.getId() == INotesErrorConstants.ERR_NSF_COMPUTE_NOENVIRONMENT);
+      }
+
+      {
+        client.getDominoRuntime().setProperty("$jnx_test", "");
+        
+        DominoException e = null;
+        try {
+          Formula formula = client
+              .createFormula("@SetEnvironment(\"jnx_test\"; \"123\")")
+              .disallow(Disallow.UNSAFE);
+          formula.evaluate();
+        }
+        catch (DominoException e1) {
+          e = e1;
+        }
+
+        assertNotNull(e);
+        assertTrue(e.getId() == INotesErrorConstants.ERR_NSF_COMPUTE_NOENVIRONMENT);
+      }
+
+      //check if FIELD fieldname:= can be prevented
+      {
+        doc.replaceItemValue("field1", "");
+        
+        Formula formula = client
+            .createFormula("FIELD field1:=\"123\"; \"\"");
+        formula.evaluate(doc);
+
+        String fldValue = doc.get("field1", String.class, "");
+        assertEquals("123", fldValue);
+      }
+      
+      {
+        doc.replaceItemValue("field1", "");
+        
+        Formula formula = client
+            .createFormula("FIELD field1:=\"123\"; \"\"")
+            .disallow(Disallow.ASSIGN);
+        formula.evaluate(doc);
+
+        String fldValue = doc.get("field1", String.class, "");
+        assertEquals("", fldValue);
+      }
+
+      {
+        doc.replaceItemValue("field1", "");
+        
+        Formula formula = client
+            .createFormula("FIELD field1:=\"123\"; \"\"")
+            .disallow(Disallow.UNSAFE);
+        formula.evaluate(doc);
+
+        String fldValue = doc.get("field1", String.class, "");
+        assertEquals("", fldValue);
+      }
+      
+      {
+        doc.replaceItemValue("field1", "");
+        
+        Formula formula = client
+            .createFormula("@SetField(\"field1\"; \"123\")")
+            .disallow(Disallow.UNSAFE);
+        formula.evaluate(doc);
+
+        String fldValue = doc.get("field1", String.class, "");
+        assertEquals("", fldValue);
+      }
+      
+      {
+        Formula formula = client
+            .createFormula("@GetDocField(\""+otherDocUnid+"\";\"form\")");
+        List<Object> result = formula.evaluate(doc);
+        assertEquals(Arrays.asList("Testform"), result);
+      }
+      
+      //FALLBACK_EXT seems to block retrieval of the doc field
+      {
+        Formula formula = client
+            .createFormula("@GetDocField(\""+otherDocUnid+"\";\"field1\")")
+            .disallow(Disallow.FALLBACK_EXT);
+        List<Object> result = formula.evaluate(doc);
+        assertEquals(Arrays.asList(""), result);
+      }
+      
+      //check if we can prevent @SetDocField
+      {
+        Formula formula = client
+            .createFormula("@SetDocField(\""+otherDocUnid+"\"; \"field1\"; \"123\")");
+        formula.evaluate(doc);
+        //we tried to load the doc to see if field1 has been set, but that
+        //was not the case; not sure why this is not working; but setting one of the
+        //Disallow constants below does even more, it throws an exception
+      }
+      
+      {
+        DominoException e = null;
+        try {
+          Formula formula = client
+              .createFormula("@SetDocField(\""+otherDocUnid+"\"; \"field1\"; \"123\")")
+                      .disallow(Disallow.UNSAFE);
+          formula.evaluate(doc);
+        } catch (DominoException e1) {
+          e = e1;
+        }
+
+        assertNotNull(e);
+        assertTrue(e.getId() == INotesErrorConstants.ERR_FUNCTION_CONTEXT);
+      }
+
+      {
+        DominoException e = null;
+        try {
+          Formula formula = client
+              .createFormula("@SetDocField(\""+otherDocUnid+"\"; \"field1\"; \"123\")")
+                      .disallow(Disallow.SIDEEFFECTS);
+          formula.evaluate(doc);
+        } catch (DominoException e1) {
+          e = e1;
+        }
+
+        assertNotNull(e);
+        assertTrue(e.getId() == INotesErrorConstants.ERR_FUNCTION_CONTEXT);
+      }
+
+    });
+    
   }
 }
