1bda6d80b74c32ffc4c8a73d5c0cd6d037784111
Jesse Gallagher <jesse@secondfoundation.org>
2023-11-13 09:13:08 -0500

    Add stub classes to aid JavaAddin compilation to lsxbeshim module

---
+++ integration/domino-jnx-lsxbeshim/src/main/java/lotus/notes/addins/JavaServerAddin.java
@@ -0,0 +1,29 @@
+package lotus.notes.addins;
+
+import lotus.domino.NotesThread;
+
+public class JavaServerAddin extends NotesThread {
+  public static final String MSG_Q_PREFIX = "MQ$";
+  
+  protected int AddInCreateStatusLine(String str) {
+    return 0;
+  }
+  
+  protected void AddInDeleteStatusLine(int statusLine) {
+    
+  }
+  
+  public native void AddInLogErrorText(String text, int status);
+  
+  protected void AddInLogMessageText(String text) {
+    
+  }
+  
+  protected synchronized boolean addInRunning() {
+    return false;
+  }
+  
+  protected native void AddInSetStatusLine(int statusLine, String text);
+  
+  protected native void OSPreemptOccasionally();
+}
---
+++ integration/domino-jnx-lsxbeshim/src/main/java/lotus/notes/internal/MessageQueue.java
@@ -0,0 +1,21 @@
+package lotus.notes.internal;
+
+public class MessageQueue {
+  public static final int MQ_WAIT_FOR_MSG = 1;
+  
+  public int close(int flags) {
+    return 0;
+  }
+  
+  public int create(String name, int a, int b) {
+    return 0;
+  }
+  
+  public int get(StringBuffer buf, int a, int b, int c) {
+    return 0;
+  }
+  
+  public int open(String name, int flags) {
+    return 0;
+  }
+}
