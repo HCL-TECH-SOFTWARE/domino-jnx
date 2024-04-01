147b8c5a8dc3a4b3f822c1556f45b66fcf23b7e7
Jesse Gallagher <jesse@secondfoundation.org>
2024-04-01 09:55:21 -0400

    Add StdNames constant for web file listing

--- domino-jnx-api/src/main/java/com/hcl/domino/constants/StdNames.java
+++ domino-jnx-api/src/main/java/com/hcl/domino/constants/StdNames.java
@@ -27,6 +27,8 @@ public interface StdNames {
   String DFLAGPAT_SHARED_COLS = "(+-*^"; //$NON-NLS-1$
   /** xsp pages */
   String DFLAGPAT_XSPPAGE = "*gK"; //$NON-NLS-1$
+  /** list of web only files */
+  String DFLAGPAT_FILE_WEB = "(+g-K[];w`,*";
   /** display only stylekits */
   String DFLAGPAT_STYLEKIT = "(+-*g`"; //$NON-NLS-1$
   /** display only widgets */
