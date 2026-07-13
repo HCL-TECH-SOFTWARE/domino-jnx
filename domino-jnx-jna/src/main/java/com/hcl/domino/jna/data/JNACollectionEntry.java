88f2d25f4acb0d28b2d54e75a57b7d0b673365af
Jesse Gallagher <jesse@secondfoundation.org>
2026-07-13 11:07:12 -0400

    Replace a single-character string with a char in getPosition

--- domino-jnx-jna/src/main/java/com/hcl/domino/jna/data/JNACollectionEntry.java
+++ domino-jnx-jna/src/main/java/com/hcl/domino/jna/data/JNACollectionEntry.java
@@ -368,7 +368,7 @@ public class JNACollectionEntry extends AbstractCollectionEntry {
 					StringBuilder sb = new StringBuilder();
 					for (int i=0; i<m_pos.length; i++) {
 						if (i>0) {
-							sb.append("."); //$NON-NLS-1$
+							sb.append('.');
 						}
 						sb.append(m_pos[i]);
 					}
