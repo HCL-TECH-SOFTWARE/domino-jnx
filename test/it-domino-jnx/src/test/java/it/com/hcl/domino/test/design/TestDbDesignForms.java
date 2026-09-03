f4150de4c49c3e6853de886fd4cbef3f0f1f3168
Jesse Gallagher <jesse@secondfoundation.org>
2026-09-03 12:35:36 -0400

    Fix JUnit import

--- test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/design/TestDbDesignForms.java
+++ test/it-domino-jnx/src/test/java/it/com/hcl/domino/test/design/TestDbDesignForms.java
@@ -17,11 +17,11 @@
 package it.com.hcl.domino.test.design;
 
 import static it.com.hcl.domino.test.util.ITUtil.toLf;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.net.URISyntaxException;
